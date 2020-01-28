7ebb22eba4c41e23d2a49387d141bc30b2b9620e
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-28 14:00:14 -0800

    Completed 02_02

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'K';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
     }
 }
