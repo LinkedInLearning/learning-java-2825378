0dd00c51ac11ac460d68703f2bacc8060bb2b9f9
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-28 14:44:41 -0800

    Starting 02_05b

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        int studentAge = 15;
         double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
-
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
-
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(hasPerfectAttendance);
-
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
     }
 }
