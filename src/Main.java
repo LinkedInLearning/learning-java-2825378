cbeb6012537943d4c93a7c969689c863d934acbd
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 10:41:52 -0800

    Completed 04_01

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
         String choiceTwo = "jupiter";
         String choiceThree = "saturn";
 
-        String correctAnswe = choiceThree;
+        String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
         System.out.println(question);
 
         // Write a print statement giving the answer choices
         System.out.println("Choose one of the following: " +
-                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".")
+                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
 
         // Have the user input an answer
         Scanner scanner = new Scanner(System.in);
