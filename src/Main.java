be9cb8b3e5a7a110961a243a20ef78ec671abd60
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 13:42:20 -0800

    Completed 05_03

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,18 @@ public class Main {
         input.next();
         System.out.println("It's developer tea time!");
     }
+
     public static void main(String[] args) {
+        System.out.println("Welcome to your new job");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review code");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Get promoted!");
 
     }
 
