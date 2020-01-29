9d9dfb8df53e6108aab1084f08afc6a4b653937e
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 15:17:54 -0800

    Edit begin

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        double result = Math.pow(2, 5);
-        System.out.println(result);
-
-        Math.
+        
     }
 
 }
