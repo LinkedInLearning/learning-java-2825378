60ea764e769fb3cce3bd066755be084ecaddf21a
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 14:50:28 -0800

    Begin state

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
 public class Main {
 
-    public static double calculateTotalMealPrice(double listedMealPrice,
-                                               double tipRate,
-                                               double taxRate) {
-        double tip = tipRate * listedMealPrice;
-        double tax = taxRate * listedMealPrice;
-        double result = listedMealPrice + tip + tax;
-        return result;
-    }
-
     public static void main(String[] args) {
-        double groupTotalMealPrice = calculateTotalMealPrice(100, .20, .08);
-        System.out.println(groupTotalMealPrice);
-        double individualMealPrice = groupTotalMealPrice / 5;
-        System.out.println(individualMealPrice);
+        
     }
 
 }
