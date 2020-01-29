e0255570ea2f81739955ac380166d155411cc67e
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 14:47:42 -0800

    Completed 05_05

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
 
-    public static void calculateTotalMealPrice(double listedMealPrice,
+    public static double calculateTotalMealPrice(double listedMealPrice,
                                                double tipRate,
                                                double taxRate) {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
-        System.out.println("Your total meal price is " + result);
+        return result;
     }
 
     public static void main(String[] args) {
-        
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .20, .08);
+        System.out.println(groupTotalMealPrice);
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
     }
 
 }
