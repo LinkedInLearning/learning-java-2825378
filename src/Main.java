100ae25eca2b067b7089ef30e70924e836c30cfa
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 11:15:29 -0800

    Completed 04_02

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         // Write a print statement giving the answer choices
         System.out.println("Choose one of the following: " +
-                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".")
+                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
 
         // Have the user input an answer
         Scanner scanner = new Scanner(System.in);
