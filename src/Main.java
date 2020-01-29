8740322cd719aeea9cb4c8fc7fa19d6511e4fa58
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-28 17:03:32 -0800

    Completed 03_03

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+
+        int inputtedNum = scanner.nextInt();
+
+        if (inputtedNum < 5) {
+            System.out.println("Enjoy the good luck a friend brings you");
+        } else {
+            System.out.println("Your shoe selection will make you very happy today");
+        }
     }
 }
