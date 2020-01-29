668c4f22ce8f5bc700392f7ba342cb3c9e010993
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 14:05:33 -0800

    Edit begin

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
-    public static void calculateTotalMealCost() {
-        
+    public static void calculateTotalMealPrice() {
     }
 
     public static void main(String[] args) {
