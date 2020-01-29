da271dbb7400d1d925a8703bed311204ea13fe07
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 11:00:13 -0800

    Starting 04_02b

--- src/Main.java
+++ src/Main.java
@@ -8,30 +8,25 @@ public class Main {
         String choiceTwo = "jupiter";
         String choiceThree = "saturn";
 
-        String correctAnswer = choiceTwo;
+        String correctAnswer = choiceThree;
 
         // Write a print statement asking the question
         System.out.println(question);
 
         // Write a print statement giving the answer choices
         System.out.println("Choose one of the following: " +
-                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
+                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".")
 
         // Have the user input an answer
         Scanner scanner = new Scanner(System.in);
         // Retrieve the user's input
         String input = scanner.next();
 
-        // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
         if(correctAnswer.equals(input.toLowerCase())) {
             System.out.println("Congrats! That's the correct answer");
         } else {
             System.out.println("You are incorrect. The correct answer is " + correctAnswer);
         }
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
     }
 
 }
