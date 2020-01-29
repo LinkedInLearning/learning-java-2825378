7e6723c2e92f00707bd2001a3eb47903bb498906
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 13:47:38 -0800

    Begin state

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void announceDeveloperTeaTime() {
-        System.out.println("Waiting for developer tea time...");
-        System.out.println("Type in a random word and press Enter to start developer tea time");
-        Scanner input = new Scanner(System.in);
-        input.next();
-        System.out.println("It's developer tea time!");
+    public static void calculateTotalMealCost() {
+        
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to your new job");
-
-        announceDeveloperTeaTime();
-
-        System.out.println("Write code");
-        System.out.println("Review code");
-
-        announceDeveloperTeaTime();
-
-        System.out.println("Get promoted!");
 
     }
 
