be9c20f38865abc0d4b5e847ecee02a06072f387
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 15:17:21 -0800

    End

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 public class Main {
 
-    public static void main(String[] args) {
-        double result = Math.pow(2, 5);
-        System.out.println(result);
+    public static double salaryCalculator(double hoursPerWeek,
+                                          double amountPerHour,
+                                          int vacationDays) {
+        if (hoursPerWeek < 0) {
+            return -1;
+        }
 
-        Math.
+        if (amountPerHour < 0) {
+            return -1;
+        }
+
+        double weeklyPaycheck = hoursPerWeek * amountPerHour;
+        double unpaidTime = vacationDays * amountPerHour * 8;
+        return (weeklyPaycheck * 52) - unpaidTime;
+    }
+
+    public static void main(String[] args) {
+        double salary = salaryCalculator(40, 15, 8);
+        System.out.println(salary);
     }
 
 }
