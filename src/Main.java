ccc5f33d1268480f4a78554e0b9e619082246753
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-28 16:05:33 -0800

    Completed 02_06

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println(studentFirstName + " " +
+                studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName +
+                " now has a GPA of " + studentGPA);
     }
 }
