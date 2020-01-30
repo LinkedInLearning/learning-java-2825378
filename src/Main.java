e0860ed1c0f52556e49c641b0d7cb04ccaf5729a
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 16:10:24 -0800

    Completed 06_04

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triangleB = new Triangle(3, 2.598, 3, 3,3);
 
     }
 
