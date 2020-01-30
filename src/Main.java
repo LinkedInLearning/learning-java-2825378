7f2550da8a7f43df6604d33d6ccdadeecffcbfc8
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-30 10:47:14 -0800

    Update end

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        double groupTotalMealPrice = calculateTotalMealPrice(100, .20, .08);
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, .08);
         System.out.println(groupTotalMealPrice);
+
         double individualMealPrice = groupTotalMealPrice / 5;
         System.out.println(individualMealPrice);
     }
