d5cdfd60956191f10107c5385aae5bde79fff206
Kathryn Hodge <kathrynhodge@car-booth-10-mac.content.linkedin.biz>
2020-01-29 16:20:33 -0800

    Completed 06_05

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
         Triangle triangleB = new Triangle(3, 2.598, 3, 3,3);
 
+        double triangleAArea = triangleA.findArea();
+        System.out.println(triangleAArea);
+
+        // Triangle.findArea()? --> Math.pow(2,3)?
+
+        
     }
 
 }
