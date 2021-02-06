3bbb6cd2f0b9f3176315b55c2a0f780437b211c6
mckeejl13 <78573162+mckeejl13@users.noreply.github.com>
2021-02-06 12:44:04 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 15;
+        double studentGPA = 3.5;
+        String studentName = "JenniferMcKee";
+        char studentFirstInitial = studentName.charAt(0);
+        char studentLastInitial = studentName.charAt(8);
+        boolean hasPerfectAttendance = true;
+        System.out.println(studentAge);
     }
 }
