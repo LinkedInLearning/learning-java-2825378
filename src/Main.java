90ddef1bffeb4a5b360675c604bb899a950d76a9
VelasquezTracy25 <velasqueztracy25@gmail.com>
2021-03-23 22:08:03 -0500

    Completed exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char firstInitial = 'T';
+        char lastInitial = 'V';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentGPA);
     }
 }
