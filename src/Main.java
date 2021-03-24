d8abfe3600eeb20eefb682658cbc5ff563cede03
VelasquezTracy25 <velasqueztracy25@gmail.com>
2021-03-23 22:44:24 -0500

    Completed exercise

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
+
     public static void main(String[] args) {
-        
+        System.out.println(salaryCalc(40, 10.40, 4));
+        System.out.println(salaryCalc(0, 10.40, 4));
+    }
+
+    public static double salaryCalc(double hours, double hourlyPay, double vacationDays){
+        double weeklyPaycheck = hours * hourlyPay;
+        double vacationDeduction = vacationDays*8*hourlyPay;
+
+        if (hours < 0 || vacationDays < 0){
+            return -1;
+        }
+
+        double salary = (weeklyPaycheck * 52) - vacationDeduction;
+        return salary;
     }
 
 }
