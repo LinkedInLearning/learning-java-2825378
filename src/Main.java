fc2a91783bea0c07d9d1c40ec7e992a02a299754
Khervyn Gupta <88488547+Khervyngupta@users.noreply.github.com>
2022-09-20 21:11:54 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please input the base of the triangle (in inches).");
-        double base = input.nextDouble()
+        double base = input.nextDouble();
 
         while (base <= 0) {
             System.out.println("That's invalid. Please input the base of the triangle (in inches).");
@@ -20,11 +20,11 @@ public class Main {
         double height = input.nextDouble();
         while (height <= 0) {
             System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            base = input.nextDouble();
+            height = input.nextDouble();
         }
 
         double area = (base * height) / 2;
-        System.out.println("The area is " + height);
+        System.out.println("The area is " + area);
 
     }
 }
