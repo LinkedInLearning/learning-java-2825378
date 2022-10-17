bb6d393f55942cbdc1e194e6ce6f044298ef6838
patwyzlic <patrickwyzlic@gmail.com>
2022-10-17 14:52:29 -0400

    Check for answer with User Input

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "Largest Planet?";
+        String choiceOne = "Earth";
+        String choiceTwo = "Jupiter";
+        String choiceThree = "Saturn";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+        System.out.println("Choose one: " + choiceOne + ", " + choiceTwo + " " + choiceThree);
         // Have the user input an answer
+        Scanner scanner = new Scanner(System.in);
         // Retrieve the user's input
+        String input = scanner.next();
 
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
+        if(correctAnswer.equals(input.toLowerCase())){
+            System.out.println("Congrats!");
+        }else{
+            System.out.println("Incorrect!");
+        }
+
 
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
