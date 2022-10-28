643156dc83b35ff126f39a8ec9459ec706c7d2f7
Kenny Hughes <kenhughes@linkedin.com>
2022-10-28 16:31:55 -0600

    triangles exercise completed

--- src/triangles.java
+++ src/triangles.java
@@ -2,9 +2,16 @@ public class triangles {
 
     public static void main(String[] args) {
         Triangle triangleA = new Triangle(3, 2.5, 3, 3, 3);
-        Triangle traingleB = new Triangle(1,1,1,1,1);
+        Triangle triangleB = new Triangle(1,1,1,1,1);
 
         double triangleAArea = triangleA.findArea();
         System.out.println(triangleAArea);
+
+        double triangleBArea = triangleB.findArea();
+        System.out.println(triangleBArea);
+
+        System.out.println(triangleA.sideLenThree);
+        System.out.println(triangleB.base);
+        System.out.println(Triangle.numOfSides);
     }
 }
